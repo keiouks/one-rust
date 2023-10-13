538d3cb7da2e526b1bd406d83f69dd4d0e5359d5
keiouksliu <keiouksliu@futunn.com>
2023-10-13 09:44:19 +0800

    feat: first commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
+fn main() {
+  let stdout = stdout();
+  let message = String::from("beginning");
+  let width = message.chars().count();
+
+  let mut writer = BufWriter::new(stdout.lock());
+  say(&message, width, &mut writer).unwrap();
+}
