df2e021ffe338c2f509d1449828caf6691707f04
keiouksliu <keiouksliu@futunn.com>
2024-01-17 09:36:15 +0800

    feat(http): 请求接收

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
+use std::{
+  fs,
+  io::{prelude::*, BufReader},
+  net::{TcpListener, TcpStream},
+};
 
 fn main() {
-  let mut s = String::from("hello world");
-  let word = first_word(&s);
-  s.clear();
-  println!("{}", word);
+  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+  for stream in listener.incoming() {
+    let stream: std::net::TcpStream = stream.unwrap();
+    handle_connection(stream);
+  }
 }
 
-fn first_word(s: &String) -> &str {
-  let bytes = s.as_bytes();
+fn handle_connection(mut stream: TcpStream) {
+  let buf_reader = BufReader::new(&mut stream);
+  let request_line = buf_reader.lines().next().unwrap().unwrap();
+  
+  if request_line == "GET / HTTP/1.1" {
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    let response = 
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+  } else {
+    let status_line = "HTTP/1.1 404 NOT FOUND";
+    let contents = fs::read_to_string("404.html").unwrap();
+    let length = contents.len();
 
-  for (i, &item) in bytes.iter().enumerate() {
-      if item == b' ' {
-          return &s[0..i];
-      }
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
   }
-
-  &s[..]
 }
