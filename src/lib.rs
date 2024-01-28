58648f030d69d72886f77a9802b48a492985a950
keiouks <13428969714@163.com>
2024-01-28 22:32:14 +0800

    feat(Arc-mutex): thread-pool用channel,而recever用互斥锁mutex共享于多个thread之间,所以这个mutex也要用原子引用计数来做内存管理

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+use std::{
+  sync::{mpsc, Arc, Mutex},
+  thread,
+};
+
+struct Worker {
+  id: usize,
+  thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    let thread = thread::spawn(|| {
+      receiver;
+    });
+    Worker { id, thread }
+  }
+}
+
+struct Job;
+
+pub struct ThreadPool {
+  workers: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+  pub fn new(size: usize) -> ThreadPool {
+    assert!(size > 0);
+    let (sender, receiver) = mpsc::channel();
+    let receiver = Arc::new(Mutex::new(receiver));
+    let mut workers = Vec::with_capacity(size);
+    for id in 0..size {
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
+    }
+    ThreadPool { workers, sender }
+  }
+
+  pub fn execute<F>(&self, f: F)
+  where
+    F: FnOnce() + Send + 'static,
+  {
+
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,43 @@ use std::{
   thread,
   time::Duration,
 };
+use one_rust::ThreadPool;
 
 fn main() {
   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  let pool = ThreadPool::new(4);
 
   for stream in listener.incoming() {
     let stream: std::net::TcpStream = stream.unwrap();
-    handle_connection(stream);
+
+    pool.execute(|| {
+      handle_connection(stream);
+    });
   }
 }
 
 fn handle_connection(mut stream: TcpStream) {
   let buf_reader = BufReader::new(&mut stream);
-  let request_line = buf_reader.lines().next().unwrap().unwrap();
-  
-  let (status_line, filename) = match &request_line[..] {
-    "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-    "GET /sleep HTTP/1.1" => {
-      thread::sleep(Duration::from_secs(7));
-      ("HTTP/1.1 200 OK", "hello.html")
+  let status_line:&str;
+  let filename:&str;
+  match buf_reader.lines().next() {
+    None => {(status_line, filename) = ("HTTP/1.1 404 NOT FOUND", "404.html")},
+    Some(result) => {
+      match result {
+        Err(_e) => {(status_line, filename) = ("HTTP/1.1 404 NOT FOUND", "404.html")},
+        Ok(request_line) => {
+          (status_line, filename) = match &request_line[..] {
+            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+            "GET /sleep HTTP/1.1" => {
+              thread::sleep(Duration::from_secs(7));
+              ("HTTP/1.1 200 OK", "hello.html")
+            }
+            _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+          };
+        } 
+      }
     }
-    _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-  };
+  }
   
   let contents = fs::read_to_string(filename).unwrap();
   let length = contents.len();
